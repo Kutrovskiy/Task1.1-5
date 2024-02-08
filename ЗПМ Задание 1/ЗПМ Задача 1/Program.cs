be98a715a7e94d7709a11177ea07045d45a1205f
Kutrovskiy <157221613+Kutrovskiy@users.noreply.github.com>
2024-02-08 14:55:58 +0300

    остальные задания

---
+++ ЗПМ Задание 1/ЗПМ Задача 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Заданный массив целых чисел
+        int[] numbers = { 2, 7, 15, 8, 3, 12, 5, 10 };
+
+        Console.WriteLine("Нечетные числа в массиве:");
+
+        // Перебор элементов массива и вывод нечетных чисел
+        foreach (int number in numbers)
+        {
+            if (IsOdd(number))
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+
+    static bool IsOdd(int number)
+    {
+        return number % 2 != 0;
+    }
+}
